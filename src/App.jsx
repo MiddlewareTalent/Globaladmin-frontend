705b81fd9867b9a250fa4805e467320b708919b2
MTL1014 <ravitejaganji49@gmail.com>
2025-04-09 15:34:36 +0530

    app.js changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import "./App.css";
+import { BrowserRouter, Routes,Route } from "react-router-dom";
+import Login from "./component/Admin/Login";
+import AddCompany from "./component/CompanyDetails/AddCompany";
+
 
 function App() {
-  return <></>;
+  return <>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />}/>
+      <Route path="/addCompany" element={<AddCompany />} />
+    </Routes>
+    </BrowserRouter>
+    </>;
 }
 
 export default App;
